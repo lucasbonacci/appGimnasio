a7144f61ec3fa340748649411c830157f9fa41d0
lucasbonacci <lucas.bonacci96@gmail.com>
2021-06-27 14:34:03 -0300

    app finished

---
+++ js/app.js
@@ -0,0 +1,96 @@
+
+const formulario = document.querySelector('#formulario')
+const resultados = document.querySelector('#resultados')
+
+formulario.addEventListener('submit',captarDatos)
+
+function captarDatos(e){
+    e.preventDefault()
+    const altura = document.querySelector('#altura').value
+    const peso = document.querySelector('#kilos').value
+    const edad = document.querySelector('#edadd').value
+    let sexo = document.querySelector('#sexo').value
+    let actividad = document.querySelector('#actividad').value
+
+    sexo = parseInt(sexo)
+    actividad = parseInt(actividad)
+    sosHombreOMujer(sexo,peso,altura,edad,actividad)
+}
+
+
+function sosHombreOMujer(sexo,peso,altura,edad,actividad){
+    if (sexo === 1){
+        calculaCaloriasHombre(peso,altura,edad,actividad)
+    } else{
+        calculaCaloriasMujer(peso,altura,edad,actividad)
+    }
+
+}
+
+
+function calculaCaloriasHombre(peso, altura, edad, actividad){
+    const metabolismoBasal = 10*peso + 6.25*altura - 5*edad + 5
+    console.log(`tu metabolismo basal consume ${metabolismoBasal} calorias`)
+    let caloriasDiarias
+    switch (actividad){
+        case 0:
+            caloriasDiarias = metabolismoBasal * 1.200
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 1:
+            caloriasDiarias = metabolismoBasal * 1.375
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 2:
+            caloriasDiarias = metabolismoBasal * 1.550
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 3:
+            caloriasDiarias = metabolismoBasal * 1.725
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 4:
+            caloriasDiarias = metabolismoBasal * 1.900
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+    }
+    imprimirHtml(metabolismoBasal, caloriasDiarias)
+}
+
+
+function calculaCaloriasMujer(peso,altura,edad, actividad){
+    const metabolismoBasal= 10*peso + 6.25*altura - 5*edad -161
+    console.log(`tu metabolismo basal consume ${metabolismoBasal} calorias`)
+    let caloriasDiarias
+    switch (actividad){
+        case 0:
+            caloriasDiarias = metabolismoBasal * 1.200
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 1:
+            caloriasDiarias = metabolismoBasal * 1.375
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 2:
+            caloriasDiarias = metabolismoBasal * 1.550
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 3:
+            caloriasDiarias = metabolismoBasal * 1.725
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+        case 4:
+            caloriasDiarias = metabolismoBasal * 1.900
+            console.log(`tu cuerpo segun tu actividad fisica quema ${caloriasDiarias} calorias por dia`)
+            break
+    }    
+    imprimirHtml(metabolismoBasal, caloriasDiarias)
+}
+
+
+function imprimirHtml(metabolismoBasal, caloriasDiarias){
+    const resultadosHtml = document.createElement('h5')
+    resultadosHtml.textContent = `tu cuerpo en reposo quema ${metabolismoBasal} y teniendo en cuenta tu actividad fisica quema ${caloriasDiarias}`
+    resultados.appendChild(resultadosHtml)
+}
+
---
+++ js/appgrasa.js
@@ -0,0 +1,47 @@
+const formulario = document.querySelector('#formulario')
+const resultados = document.querySelector('#resultados')
+
+formulario.addEventListener('submit',captarDatos)
+
+function captarDatos(e){
+    e.preventDefault()
+    const altura = document.querySelector('#altura').value
+    const cuello = document.querySelector('#cuello').value
+    const cintura = document.querySelector('#cintura').value
+    const cadera = document.querySelector('#cadera').value
+    let sexo = document.querySelector('#sexo').value
+
+    sexo = parseInt(sexo)
+    sosHombreOMujer(sexo,altura,cintura,cuello,cadera)
+}
+
+
+function sosHombreOMujer(sexo,altura,cintura,cuello,cadera){
+    if (sexo === 1){
+        calcularGrasaHombres(altura,cintura,cuello)
+        console.log('H')
+    } else{
+        calcularGrasaMujeres(altura,cintura,cuello,cadera)
+        console.log('M')
+    }
+
+}
+
+
+
+function calcularGrasaHombres(altura,cintura,cuello){
+    const porcentajeGraso = (495/(1.0324-0.19077 * (Math.log10(cintura - cuello)) + 0.15456 * (Math.log10(altura))) - 450)
+    imprimirHtml(porcentajeGraso)
+}
+
+function calcularGrasaMujeres(altura,cintura,cuello,cadera){
+    const porcentajeGraso = (495/(1.29579-0.35004 * (Math.log10(cintura  + cadera - cuello)) + 0.22100 * (Math.log10(altura))) - 450)
+    imprimirHtml(porcentajeGraso)
+}
+
+
+function imprimirHtml(porcentajeGraso){
+    const resultadosHtml = document.createElement('h5')
+    resultadosHtml.textContent = `Tu porcentaje graso es de ${porcentajeGraso} %`
+    resultados.appendChild(resultadosHtml)
+}
